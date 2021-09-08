47f84d7bd5b39cbf1766fd2293e29f844d049bff
ilaria.source <ilaria.secchi@avanade.com>
2021-09-08 16:40:20 +0200

    sviluppo logica component converter

--- src/app/programmer/programmer.component.ts
+++ src/app/programmer/programmer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-programmer',
@@ -7,7 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProgrammerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient){}
+
+  fromHexa(num: string)
+  {
+    //converte in decimale
+    //richiama le funzioni che partono da decimale
+  }
+  fromDec(num: number)
+  {
+    //richiama le funzioni da decimale es  decToHexa(num)
+  }
+
+  fromOct(num: number)
+  {
+    //converte in decimale
+    //richiama le funzioni che partono da decimale
+  }
+
+  fromBin(num: string)
+  {
+    //converte in decimale
+    //richiama le funzioni che partono da decimale
+  }
+
+  decToHexa(num: string)
+  {}
+
+  decToOct(num: string)
+  {}
+
+  decToBin(num: string)
+  {}
 
   ngOnInit(): void {
   }
